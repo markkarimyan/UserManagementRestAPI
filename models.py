24ec7e3eafc9632304d161c3559835328ed57a86
Mark Karimyan <markkarimyan@gmail.com>
2024-05-29 21:55:13 +0400

    Initial Commit

---
+++ models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#DB Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
