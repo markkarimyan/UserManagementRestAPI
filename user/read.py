ab59fa7febcdb37c687ff433a9219e3484235e64
Mark Karimyan <markkarimyan@gmail.com>
2024-05-30 21:45:01 +0400

    Add user get by id

--- user/read.py
+++ user/read.py
@@ -13,3 +13,13 @@ def get_users():
         return jsonify({"error": str(e)}), 500
     
 
+@user_read_routes.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+    try:
+        user = User.query.get(id)
+        if user is None:
+            return jsonify({"error": "User not found"}), 404
+        result = {"id": user.id, "name": user.name, "email": user.email}
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
